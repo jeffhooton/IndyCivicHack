e5b83ed5764b9ec73ef217f45261065514e1780f
Jeff Hooton <jhooton@elevenfiftyconsulting.com>
2015-06-06 18:53:57 -0400

    Final Commit for 7:30

--- app/src/main/java/com/elevenfifty/www/indycivichack/View/CheckView.java
+++ app/src/main/java/com/elevenfifty/www/indycivichack/View/CheckView.java
@@ -2,19 +2,30 @@ package com.elevenfifty.www.indycivichack.View;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.elevenfifty.www.indycivichack.CivicApplication;
+import com.elevenfifty.www.indycivichack.R;
+import com.parse.ParseUser;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import flow.Flow;
 
 /**
  * Created by jeffhooton on 6/6/15.
  */
-public class CheckView extends RelativeLayout {
+public class CheckView extends LinearLayout {
     private Flow flow;
     private Context context;
+
+    @InjectView(R.id.firstChoiceStatus)
+    TextView firstChoice;
+    @InjectView(R.id.secondChoiceStatus)
+    TextView secondChoice;
+    @InjectView(R.id.thirdChoiceStatus)
+    TextView thirdChoice;
 
     public CheckView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -26,5 +37,11 @@ public class CheckView extends RelativeLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
         ButterKnife.inject(this);
+
+        ParseUser user = ParseUser.getCurrentUser();
+//        firstChoice.setText(user.get("statusOne").toString());
+//        secondChoice.setText(user.get("statusTwo").toString());
+//        thirdChoice.setText(user.get("statusThree").toString());
+
     }
 }
